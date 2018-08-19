8e7b01daad33825ca1c1983c4336d88c0042e8d4
RiFlowTH <riflowth@gmail.com>
2018-08-19 12:20:32 +0700

    Added comments to incomplete system

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+/*
+	Skin-System
+	https://github.com/riflowth/SkinSystem
+*/
+ 	/* Import Lib */
+	require_once __DIR__ . '/lib.php';
+	
+	/* Initial Feedback Variable */
+	$data = array();
+	$error = array();
+ 	/* Username or Password is empty */
+	if(empty($_POST["username"])){ $error["username"] = "Username is required."; }
+	if(empty($_POST["password"])){ $error["password"] = "Password is required."; }
+ 	/* Username and Password are not empty */
+	if(!empty($_POST["username"]) && !empty($_POST["password"])){
+		$username = strtolower($_POST["username"]);
+		$password = $_POST["password"];
+ 		$username_pass = false;
+		$password_pass = false;
+ 		/* Check Username */
+		$authme = authmeDBQuery("SELECT username FROM ". $config['authme']['table'] ." WHERE username = ?", [$username]);
+		$result = $authme->fetch(PDO::FETCH_ASSOC);
+ 		if($result["username"] == $username){
+			$username_pass = true;
+		} else {
+			$error["username"] = "Username is invalid.";
+		}
+ 		/* Check Password */
+		$authme = authmeDBQuery("SELECT password FROM " . $config['authme']['table'] . " WHERE username = ?", [$username]);
+		$result = $authme->fetch(PDO::FETCH_ASSOC);
+ 		$hashParts = explode("$", $result["password"]);
+		if(count($hashParts) === 4){
+			if(hash("sha256", hash("sha256", $password) . $hashParts[2]) === $hashParts[3]){
+				$password_pass = true;
+			} else {
+				/* Password is not correct */
+				$error["password"] = "Password is invalid.";
+				/* Create PasswordCaching table if not exist */
+				//$data = skinsystemDBQuery("");
+				//$result = $data->fetch(PDO::FETCH_ASSOC);
+			}
+		}
+ 		/* Username and Password are correct */
+		if($username_pass && $password_pass){
+			session_start();
+			$_SESSION["username"] = $username;
+			$data["success"] = true;
+		}
+	}
+ 	/* Assign error to data array. When it has some error. */
+	if($error){
+		$data["success"] = false;
+		$data["error"] = $error;
+	}
+ 	echo json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+?>
