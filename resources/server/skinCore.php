3cc76fcc2e3f4737c11de267893d0de97a6ba179
RiFlowTH <17198802+riflowth@users.noreply.github.com>
2019-02-17 21:30:58 +0700

    [Added] Simple skinCore

--- resources/server/skinCore.php
+++ resources/server/skinCore.php
@@ -1,3 +1,9 @@
 <?php
-  
+  require_once(__DIR__ . '/libraries.php');
+
+  if(!empty($_POST['username']) && !empty($_POST['isSlim']) && !empty($_POST['uploadtype'])){
+    printDataAndDie();
+  }
+
+  printErrorAndDie('Invalid Request!');
  ?>
