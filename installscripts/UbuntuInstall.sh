93d06af8346f8ab3c6678d1a004f0618cc7e222c
ITZVGcGPmO <42504016+ITZVGcGPmO@users.noreply.github.com>
2019-04-26 11:14:40 -0700

    use latest stable release

--- installscripts/UbuntuInstall.sh
+++ installscripts/UbuntuInstall.sh
@@ -5,9 +5,12 @@ apt-get update
 apt-get install mysql-server apache2 libapache2-mod-php php-curl php-mysql php-gd git -y
 cd /var/www
 git clone https://github.com/riflowth/SkinSystem
-rm -rf SkinSystem/.git
-rm -rf SkinSystem/.gitignore
-rm -rf SkinSystem/*.md
+cd SkinSytem
+git checkout `git tag | sort -V | grep -v "\-rc" | tail -1`
+rm -rf .git
+rm -rf .gitignore
+rm -rf *.md
+cd ..
 mv html html.backup$(date -I)
 mv SkinSystem html
 chmod 775 -R /var/www/html && chown -R www-data:www-data /var/www/html
